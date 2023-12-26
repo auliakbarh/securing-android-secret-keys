0740bd5a671208e84dcb372bcf7724c6bedd0d63
auliakbarh <auliaakbarharahaap@gmail.com>
2023-12-26 23:57:45 +0700

    retrieve secrets from secrets.xml

--- app/src/main/java/com/example/secureapp/MainActivity.kt
+++ app/src/main/java/com/example/secureapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.secureapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,14 +13,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.secureapp.ui.theme.SecureAppTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // inside of an Activity, `getString` is called directly
+        // inside of an Activity, `getString` is called directly
+        val baseUrl: String = getString(R.string.base_url)
+        val googleMapApiKey: String = getString(R.string.google_maps_api_key)
+        // inside of another class (requires a context object to exist)
+        // inside of another class (requires a context object to exist)
+        // val secretValue: String = context.getString(R.string.base_url)
         setContent {
             SecureAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting(baseUrl, googleMapApiKey)
                 }
             }
         }
@@ -27,17 +36,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
+fun Greeting(baseUrl: String, googleMapApiKey: String, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Exposing Base URL: $baseUrl",
             modifier = modifier
-    )
+        )
+        Text(
+            text = "Exposing Google API Key: $googleMapApiKey",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SecureAppTheme {
-        Greeting("Android")
+        Greeting("Default Base URL", "Google Map API Key")
     }
 }
