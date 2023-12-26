6e40c544328ef99a1f92eb2b9a59bc7ab67e6ccb
auliakbarh <auliaakbarharahaap@gmail.com>
2023-12-27 02:44:33 +0700

    use secrets in kotlin file

--- app/src/main/java/com/example/secureapp/MainActivity.kt
+++ app/src/main/java/com/example/secureapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.secureapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,11 +16,13 @@ import com.example.secureapp.ui.theme.SecureAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val apiKey = Secrets().getAPI_KEY(packageName)
+        val baseUrl = Secrets().getBASE_URL(packageName)
         setContent {
             SecureAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting(apiKey, baseUrl)
                 }
             }
         }
@@ -27,17 +30,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
+fun Greeting(apiKey: String, baseUrl: String, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Api Key: $apiKey",
             modifier = modifier
-    )
+        )
+        Text(
+            text = "Base URL: $baseUrl",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SecureAppTheme {
-        Greeting("Android")
+        Greeting("Default API Key", "Default Base URL")
     }
 }
